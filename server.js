e1528f7cd920027fff327b62a20b324735c98220
Thomas Jensen <thomasjensen@Thomass-MacBook-Pro.local>
2019-07-07 18:21:54 -0500

    fixed listener for connection issue

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 var express = require("express");
 var exphbs = require("express-handlebars");
 var mysql = require("mysql");
+var bodyParser = require("body-parser");
 
 
-var orm = require("./config/orm.js");
+var app = express();
+var port = process.env.PORT || 3000;
 
-// Find all the pets ordering by the lowest price to the highest price.
-orm.selectAndOrder("animal_name", "pets", "price");
+app.listen(port,function(){
+    console.log("Server is listening on http://localhost:" + port);
+}); 
 
-// Find a pet in the pets table by an animal_name of Rachel.
-orm.selectWhere("pets", "animal_name", "Rachel");
 
-// Find the buyer with the most pets.
-orm.findWhoHasMost("buyer_name", "buyer_id", "buyers", "pets");
+
  
