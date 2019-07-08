d8d8976553a4834f05b305684b8f57893b02fc5e
Thomas Jensen <thomasjensen@Thomass-MacBook-Pro.local>
2019-07-07 19:33:14 -0500

    finally i think grrrrr

--- server.js
+++ server.js
@@ -2,33 +2,17 @@ var express = require("express");
 var exphbs = require("express-handlebars");
 var mysql = require("mysql");
 var bodyParser = require("body-parser");
+var port = process.env.PORT || 3000;
+var app = express();
 
-
-// app.use(express.static("public"));
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/catsController.js");
-
-app.use(routes);
-
-
-var app = express();
-var port = process.env.PORT || 3000;
-
-
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-var exphbs = require("express-handlebars");
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
 var routes = require("./controllers/catsController.js");
 
 app.use(routes);
