4d94f14c4cc8c2bcd8958dfb2d9e11c6445a0b83
Thomas Jensen <thomasjensen@Thomass-MacBook-Pro.local>
2019-07-10 19:07:50 -0500

    finished helpers

--- config/orm.js
+++ config/orm.js
@@ -10,6 +10,21 @@ function printQuestionMarks(num) {
   return arr.toString();
 } 
 
+function objToSql(obj){
+  var arr= [];
+  for (var key in ob){
+    var value = ob[key];
+    if (Object.hasOwnProperty.call(ob,key)){
+      if(typeof value === "string" && value.indexOf(" ")>=0){
+        value = "'" + value + "'";
+      }
+      arr.push(key + "=" + value);
+    }
+
+  }
+  return arr.toString();
+}
+
 var orm = {
   selectAll: function(table,cb) {
     var queryString = "SELECT * FROM" + table + ";";
