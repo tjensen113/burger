a9aa309a2da685f583558f4dca35cc715c189e99
Thomas Jensen <thomasjensen@Thomass-MacBook-Pro.local>
2019-07-15 18:25:23 -0500

    made last router

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -18,8 +18,20 @@ var router = express.Router();
              [req.body.burger_name, req.body.devoured],
              function(res){
                  res.json({id: result.insertId});
-                 
+
              }
-         )
+         );
+     });
+     router.put("/api/burgers/id:", function(req,res){
+         burger.updateOne({devoured: req.body.devoured}, condition,function(result){
+             if ((result, changedRows === 0)){
+                 return res.status(404).end();
+                 }
+                 else res.status(200).end();
+         }
+     )
      })
+
+
  })
+ module.exports = router;
